8c6d183c6f71a198af10a91cc9700eca2a9c9dec
unknown <milena.araujo1102@gmail.com>
2024-09-10 01:15:53 -0300

    adicionando modal e configurando intranet

--- adm_dashboard/main.js
+++ adm_dashboard/main.js
@@ -11,3 +11,25 @@ function activeLink(){
 
 list.forEach(item => item.addEventListener("mouseover", activeLink))
 
+/*===============MODAL=======================*/
+
+// Abrir o modal
+const modal = document.getElementById("modal");
+const btnEditar = document.querySelector(".buttonea button:first-child");
+const spanClose = document.querySelector(".close");
+
+btnEditar.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Fechar o modal ao clicar no X
+spanClose.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Fechar o modal ao clicar fora da área do modal
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
